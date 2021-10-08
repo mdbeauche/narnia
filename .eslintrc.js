62315aa070b697084914800742937a8797b18e08
mdbeauche <michael.beauchemin@gmail.com>
2021-10-07 20:39:47 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    babelOptions: {
+      configFile: './babel.config.json',
+    },
+  },
+  extends: ['airbnb', 'prettier'],
+  globals: { fetch: false },
+  plugins: ['prettier'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+      alias: {
+        map: [['src', './src']],
+      },
+    },
+  },
+  rules: {
+    'no-console': 'off',
+    'max-len': [2, 100, 2],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'function-paren-newline': 0,
+    'import/prefer-default-export': 0,
+    'no-trailing-spaces': ['error', { skipBlankLines: true }],
+    'no-underscore-dangle': 0,
+    'class-methods-use-this': 'off',
+    'arrow-parens': 'off',
+    'no-param-reassign': 0,
+    'no-use-before-define': ['error', { functions: false, classes: false }],
+    'no-restricted-syntax': 0,
+    'prettier/prettier': 'error',
+    'no-unused-expressions': 'error',
+    'import/no-unresolved': [2, { caseSensitive: false }],
+  },
+};
